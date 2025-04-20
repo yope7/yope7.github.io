923c0834153effa63877e7bfc74ddd6d6f1cef28
yope7 <yope_0920@outlook.com>
2025-04-20 23:42:14 +0900

    bug fix

--- create_post.sh
+++ create_post.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 引数からタイトルを取得（デフォルトは"title"）
+TITLE=${1:-title}
+
 # 今日の日付を取得
 TODAY=$(date +"%Y-%m-%d")
 
@@ -7,12 +10,15 @@ TODAY=$(date +"%Y-%m-%d")
 TEMPLATE="_posts/2001-09-20-title.md"
 
 # 新しいファイル名
-NEW_FILE="_posts/${TODAY}-title.md"
+NEW_FILE="_posts/${TODAY}-${TITLE}.md"
 
 # テンプレートをコピー
 cp "$TEMPLATE" "$NEW_FILE"
 
 # ファイル内の日付を更新
 sed -i '' "s/0000-00-00/$TODAY/g" "$NEW_FILE"
+
+# ファイル内のタイトルを更新
+sed -i '' "s/title: \"title\"/title: \"$TITLE\"/g" "$NEW_FILE"
 
 echo "新しい投稿ファイルが作成されました: $NEW_FILE" 
